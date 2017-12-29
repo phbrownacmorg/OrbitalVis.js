43e2ee1712026c81d8d24c137b0a2ab0fca586aa
Peter Brown <phbrown@acm.org>
2017-12-29 03:49:11 +0000

    Properties put into data.js

--- Controller.js
+++ Controller.js
@@ -1,5 +1,16 @@
-
+/* global $ */
 
 $(document).ready(function() {
-    
+    console.log('Doc ready');
+    //console.log(JSON.stringify($(document).data('properties')));
+    var properties = $(document).data('properties');
+
+    $('#model-select').change(function(evt) {
+        switch (evt.target.value) {
+            case 'SN2':
+                break;
+        }
+    });
+
+    console.log('End Doc ready');    
 });
---
+++ data.js
@@ -0,0 +1,6 @@
+/* global $ */
+
+$(document).data('properties', 
+                    {  "default":  { "fov": 16, "eye": [2.5, 2.5, 10], "layout": "horizontal" },
+                        "SN2":      { }
+                    });
