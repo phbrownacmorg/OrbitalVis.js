ab5d68ac275187d20818f9ec0971aaa494d27b91
Peter Brown <peter.brown@converse.edu>
2018-08-07 22:52:11 -0400

    Finished EA2A modeling

--- EA2A.js
+++ EA2A.js
@@ -42,16 +42,54 @@ function makeEA2A(props) {
     top_carb.addToOrbital(1, bottom_carb, SP3_SP3_BOND_LENGTH);
     model.needsUpdates.push(bottom_carb);
 
-    bottom_H = makeSAtom('H');
+    const bottom_H = makeSAtom('H');
     bottom_carb.addToOrbital(2, bottom_H, S_RADIUS);
     model.needsUpdates.push(bottom_H);
 
+    const ch3a = makeMethyl();
+    ch3a.rotation.set(2*Math.PI/3, 0, Math.PI);
+    top_carb.addToOrbital(2, ch3a, SP3_SP3_BOND_LENGTH);
+    model.needsUpdates.push(ch3a);
+
+    const ch3b = makeMethyl();
+    ch3b.rotation.set(-2 * Math.PI/3, 0, Math.PI);
+    bottom_carb.addToOrbital(3, ch3b, SP3_SP3_BOND_LENGTH);
+    model.needsUpdates.push(ch3b);
+   
+    const new_H = makeSAtom('H');
+    new_H.start = new THREE.Vector3(model.xSign * (S_SP3_BOND_LENGTH + 60),
+				    0, 0);
+    new_H.end = new THREE.Vector3(model.xSign * S_SP3_BOND_LENGTH, 0, 0);
+    new_H.position.copy(new_H.start);
+    model.add(new_H);
+    model.needsUpdates.push(new_H);
+
+    const cl = new SP3Atom('Cl');
+    cl.position.setX(new_H.position.x + model.xSign * S_SP3_BOND_LENGTH);
+    cl.rotateZ(Math.PI/2 + model.xSign * Math.PI/2);
+    model.add(cl);
+    model.needsUpdates.push(cl);
+
+    const top_H = makeSAtom('H');
+    top_carb.addToOrbital(3, top_H, S_RADIUS);
+    model.needsUpdates.push(top_H);
+    
     // Bonds
     const carb_carb = new Bond(top_carb, bottom_carb, DOUBLE);
     model.needsUpdates.push(carb_carb);
     // Not BACK_SLANT?
     const bottom_carb_H = new Bond(bottom_carb, bottom_H, FULL); 
     model.needsUpdates.push(bottom_carb_H);
+    const top_carb_ch3 = new Bond(top_carb, ch3a, FULL); // Not BACK_SLANT?
+    model.needsUpdates.push(top_carb_ch3);
+    const bottom_carb_ch3 = new Bond(bottom_carb, ch3b, FULL); // Not slant?
+    model.needsUpdates.push(bottom_carb_ch3);
+    const top_carb_new_H = new Bond(top_carb, new_H, BROKEN);
+    model.needsUpdates.push(top_carb_new_H);
+    const new_H_cl = new Bond(new_H, cl, FULL);
+    model.needsUpdates.push(new_H_cl);
+    const top_carb_H = new Bond(top_carb, top_H, FULL); // not slant?
+    model.needsUpdates.push(top_carb_H);
     
     model.t = 0;
     model.setT = function(newT, revQuat) {
@@ -64,17 +102,26 @@ function makeEA2A(props) {
 	      - (4 * (insideOutness - 0.5) * (insideOutness - 0.5));
 	top_carb.setInsideOutness(insideOutness);
 	top_carb.setP0Divergence(divergence);
-	bottom_carb.setP0Divergence(divergence);			  
+	bottom_carb.setP0Divergence(divergence);
+
+	new_H.position.lerpVectors(new_H.end, new_H.start,
+				   Math.max(0, 0.8 - model.t)/0.8);
 
 	// Update the bonds
 	if (model.t < 0.23) {
 	    carb_carb.setState(DOUBLE);
+	    top_carb_new_H.setState(BROKEN);
+	    new_H_cl.setState(FULL);
 	}
 	else if (model.t > 0.5) {
 	    carb_carb.setState(FULL);
+	    top_carb_new_H.setState(FULL);
+	    new_H_cl.setState(BROKEN);
 	}
 	else {
 	    carb_carb.setState(FULL_PARTIAL);
+	    top_carb_new_H.setState(PARTIAL);
+	    new_H_cl.setState(PARTIAL);
 	}
 	
         // Atoms/groups need to be updated before their bonds
