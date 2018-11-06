7a41e9586b4a61cb5efefece3283ff5892be1c92
peter.brown <peter.brown@converse.edu>
2018-11-06 15:45:31 -0500

    Made hydro2 and OH move

--- E2.js
+++ E2.js
@@ -35,9 +35,27 @@ function makeE2(model, props) {
     model.add(carb2);
     model.needsUpdates.push(carb2);
     
+    const hydro2 = new SAtom('H');
+    // Figure out the position of hydro2 relative to the center of carb1
+    hydro2.position.set((-1/3.0), Math.sin(RELAXED_ANGLE), 0);
+    hydro2.position.multiplyScalar(SP3_SP3_BOND_LENGTH + S_RADIUS);
+    // Add in the position of carb1 to find the absolute position    
+    hydro2.position.add(carb1.position);
+    hydro2.start = hydro2.position.clone();
+    hydro2.bind = hydro2.start.clone().setX(hydro2.start.x - 0.5 * S_SP3_BOND_LENGTH);
+    // Offset from OH group after bond is formed
+    hydro2.offset = new THREE.Vector3(S_SP3_BOND_LENGTH, 0, 0);
+    model.add(hydro2);
+    model.needsUpdates.push(hydro2);
+    
     const oh = new Hydroxide();
-    oh.position.set(-2 * MAX_WITHDRAWAL, 0, 0);
+    oh.position.set(-2 * MAX_WITHDRAWAL, hydro2.position.y, 0);
     oh.rotation.set(Math.PI, 0, 0);
+    oh.start = oh.position.clone();
+    oh.close1 = oh.start.clone().setX(hydro2.start.x - 1.5 * S_SP3_BOND_LENGTH - S_RADIUS);
+    oh.close2 = oh.close1.clone().setX(oh.close1.x + S_RADIUS);
+    model.add(oh);
+    model.needsUpdates.push(oh);
 
     const hydro1 = new SAtom('H');
     carb1.addToOrbital(2, hydro1, S_RADIUS);
@@ -67,11 +85,30 @@ function makeE2(model, props) {
         carb1.setP0Divergence(divergence);
         carb2.setP0Divergence(divergence);
 
-        carb1.rotation.set(0, 0, 
-                          THREE.Math.lerp(RELAXED_ANGLE,
-                                         Math.PI/2, t0));
+        carb1.rotation.set(0, 0,
+            THREE.Math.lerp(RELAXED_ANGLE,                                     Math.PI/2, t0));
         carb2.rotation.set(0, 0, 
                            Math.PI + carb1.rotation.z);
+        
+        if (newT < 0.4) {
+            oh.position.lerpVectors(oh.start, oh.close1, newT/0.4);
+            hydro2.position.copy(hydro2.start);
+        }
+        else if (newT < 0.5) {
+            oh.position.lerpVectors(oh.close1, oh.close2, 
+                                    (newT - 0.4)/0.1);
+            hydro2.position.lerpVectors(hydro2.start, hydro2.bind,
+                                        (newT - 0.4)/0.1);
+        }
+        else if (newT < 0.6) {
+            oh.position.lerpVectors(oh.close2, oh.close1,
+                                    (newT - 0.5)/0.1);
+            hydro2.position.addVectors(oh.position, hydro2.offset);
+        }
+        else {
+            oh.position.lerpVectors(oh.close1, oh.start, (newT-0.6)/0.4);
+            hydro2.position.addVectors(oh.position, hydro2.offset);
+        }
     };
     
     model.add(new THREE.AxesHelper(100));
